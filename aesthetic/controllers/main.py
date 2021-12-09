1bfa77efa5f819da1d205997d837691972953f30
aesthetic4895 <aesthetic4895@gmail.com>
2021-12-09 12:09:24 +0400

    Test autodeploy

---
+++ aesthetic/controllers/main.py
@@ -0,0 +1,8 @@
+from odoo import http
+
+from odoo.http import request
+
+class Aesthetic_employees(http.Controller):
+    @http.route('/employee', website=True, auth='public')
+    def aesthetic_employee(self, **kw):
+        return "Hello Employee"
